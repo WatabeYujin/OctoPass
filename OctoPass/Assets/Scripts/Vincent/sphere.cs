7868ee5348454519f699694e42e61dde55545f46
Vincent Yu <vantoriarin@gmail.com>
2018-09-28 20:31:22 +0900

    新しい反射仕方がやり直しました。

--- OctoPass/Assets/Scripts/Vincent/sphere.cs
+++ OctoPass/Assets/Scripts/Vincent/sphere.cs
@@ -8,48 +8,50 @@ public class sphere : MonoBehaviour {
 
     Vector3 reflectDir;
     ContactPoint contact;
-    Rigidbody rb;
-    Vector3 before_velocity;
+    Vector3 after_hit;
+    Vector3 before_hit;
+    Vector3 inMagnitude;
+    Pearlmove otherScript;
+    bool startMoving;
+    float time;
 
+    public float reflectSpeed = 1f;
     public GameObject ball;
-    // for debug
-    float speed = 15;
+    public GameObject mainBody;
+    
 
     // Use this for initialization
     void Start () {
-        rb = this.gameObject.GetComponent<Rigidbody>();
-        before_velocity = Vector3.zero;
+        otherScript = GetComponent<Pearlmove>();
+        before_hit = mainBody.transform.position;
     }
 
-    private void FixedUpdate()
-    {
-        // For debug デバッグ用
-        float movehorizontal = Input.GetAxis("Horizontal");
-        float movevertical = Input.GetAxis("Vertical");
-        Vector3 movements = new Vector3(movehorizontal, 0.0f, movevertical);
-        rb.AddForce(movements * speed);
-        before_velocity = rb.velocity;
-    }
     // Update is called once per frame
     void Update () {
-
-        // For debug デバッグ用
-        if (Input.GetKey(KeyCode.Space))
-        {
-            SceneManager.LoadScene("main");
-        }
-        if (Input.GetKey(KeyCode.O))
-        {
-            rb.velocity = transform.forward * 40;
-        }
-        if (Input.GetKey(KeyCode.P))
-        {
-            rb.velocity = -transform.forward * 40;
-        }
         Debug.DrawRay(transform.position, reflectDir * 10, Color.red);
         Debug.DrawRay(transform.position, contact.normal * 10, Color.blue);
-        Debug.DrawRay(transform.position, before_velocity * -10, Color.green);
         Debug.DrawRay(transform.position, transform.forward * 10, Color.black);
+        Debug.DrawRay(transform.position,　-inMagnitude * 10, Color.green);
+
+        //if (transform.position != Vector3.zero)
+        //{
+        //    time += Time.deltaTime;
+        //}
+        //if (time < 3) Destroy(this);
+
+
+        //バールをtranslateで動くします
+        if (startMoving)
+        {
+            transform.Translate(reflectDir * reflectSpeed * Time.deltaTime, 0);
+            time += Time.deltaTime;
+            if (time >= 3.0f)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+
+        else startMoving = false;
     }
 
     private void OnCollisionEnter(Collision touches)
@@ -64,14 +66,25 @@ public class sphere : MonoBehaviour {
         //バールが何もついてない手にあったたら反射する
         else if (touches.gameObject.tag == "tako")
         {
+            //コンフリクトしないよう別のスクリプトを無効にします。
+            otherScript.enabled = false;
+            //ぶつかる前の位置とぶつかった位置マイナスして、ぶつかるからのmagnitudeを探します。
+            after_hit = transform.position;
+            Debug.Log(touches.contacts);
+            inMagnitude = after_hit - before_hit;
             contact = touches.contacts[0];
             //calculate angle of the ball from origin    
             //バールの反射後角度を探す
-            reflectDir = Vector3.Reflect(before_velocity, contact.normal);
+            reflectDir = Vector3.Reflect(inMagnitude, contact.normal);
             // change the ball velocity to new direction velocity
-            //バールの速度が新しい角度と元々の速度を返還する
-            rb.velocity = reflectDir.normalized * 30;
-        }
+            //バールの速度が新しい角度と元々の速度を変換する
+            startMoving = true;
+        }        
+    }
+    private void OnCollisionExit(Collision touches)
+    {
+        //最小からぶつかると、新しい値を探す
+        before_hit = touches.transform.position;
     }
 
 
